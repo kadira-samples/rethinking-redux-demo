2eebb9f1de2e69025fcba53851f196caeae54b94
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-12-22 16:34:10 +0530

    Add some comments

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import Layout from './modules/layout/view';
-import rootReducer from './reducer';
 import { configureRoutes } from './routes';
+import rootReducer from './reducer';
+// This is the Layout of our app, which render it based
+// on the route actions
+import Layout from './modules/layout/view';
 
+// Apply middlewares and add support for dev tools
 const finalCreateStore = compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore);
 
+// create the store
 const store = finalCreateStore(rootReducer);
+
+// configure routes
 configureRoutes(store);
 
+// Render the the layout
 const render = () => {
   ReactDOM.render((
     <Provider store={store}>
