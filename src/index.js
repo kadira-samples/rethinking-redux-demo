cf8ee03220def929e8188e637bd6b9e6432193e7
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-12-22 15:24:11 +0530

    Add dev tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
@@ -8,7 +8,12 @@ import Layout from './modules/layout/view';
 import rootReducer from './reducer';
 import { configureRoutes } from './routes';
 
-const store = applyMiddleware(thunk)(createStore)(rootReducer);
+const finalCreateStore = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+
+const store = finalCreateStore(rootReducer);
 configureRoutes(store);
 
 const render = () => {
